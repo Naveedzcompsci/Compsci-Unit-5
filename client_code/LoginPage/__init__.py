c0931dc334aec414d5e31b5124e9072ca55c13d6
zainnav16@gmail.com <zainnav16@gmail.com>
2024-12-17 00:04:57 +0000

    Created DashboardAdmin and LoginPage; Edited settings and theme/parameters.yaml; Deleted Frame.RowTemplate1 and Frame

--- client_code/LoginPage/__init__.py
+++ client_code/LoginPage/__init__.py
@@ -1,4 +1,4 @@
-from ._anvil_designer import RowTemplate1Template
+from ._anvil_designer import LoginPageTemplate
 from anvil import *
 import anvil.server
 import anvil.users
@@ -6,9 +6,17 @@ import anvil.tables as tables
 import anvil.tables.query as q
 from anvil.tables import app_tables
 
-class RowTemplate1(RowTemplate1Template):
+
+class LoginPage(LoginPageTemplate):
   def __init__(self, **properties):
     # Set Form properties and Data Bindings.
     self.init_components(**properties)
 
+    anvil.users.login_with_form()
+
+
     # Any code you write here will run before the form opens.
+
+  def button_2_click(self, **event_args):
+    """This method is called when the button is clicked"""
+    open_form('DashboardAdmin')
